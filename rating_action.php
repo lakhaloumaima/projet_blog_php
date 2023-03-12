dbfb660d8d475209b61be43e8541668207cad314
lakhal oumaima <lakhalouma6@gmail.com>
2023-03-12 19:28:38 +0100

    up

--- rating_action.php
+++ rating_action.php
@@ -1,32 +1,40 @@
-<?php require_once("config.php");
-include("functions.php");
-if(isset($_SESSION['login_session'])){ 
-	$userid=$_SESSION['userid'];
-}
+<?php
+
+include_once("config.php");
+include_once("functions.php");
+
+session_start();
+
+$user_id=$_SESSION['user_id'];
+
 if(isset($_POST['action'])) {
 
-  $postid = $_POST['post_id'];
+  $post_id = $_POST['post_id'];
   $action = $_POST['action'];
   switch ($action) {
   	case 'like':
-    $vote_action='like';
-insert_vote($userid,$postid,$vote_action,$db);
-         break;
-  	case 'dislike':
-         $vote_action='dislike'; 
-insert_vote($userid,$postid,$vote_action,$db);
-         break;
-  	case 'unlike':
-delete_vote($userid,$postid,$db);
+      $vote_action='like';
 
-	      break;
-  	case 'undislike':
-  	delete_vote($userid,$postid,$db);
+      insert_vote($user_id,$post_id,$vote_action,$dbConn);
       break;
-  	default:
+  	case 'dislike':
+      $vote_action='dislike';
+      insert_vote($user_id,$post_id,$vote_action,$dbConn);
+      break;
+  	case 'unlike':
+      delete_vote($user_id,$post_id,$dbConn);
+
+	    break;
+  	case 'undislike':
+      delete_vote($user_id,$post_id,$dbConn);
+      break;
+    default:
+
   }
   // execute query to effect changes in the database ...
-  echo getRating($postid,$db);
+  echo getRating($post_id,$dbConn);
   exit(0);
+
 }
-?> 
+
+
